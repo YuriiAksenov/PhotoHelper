acfdd0d35a59552f793c7cd75dce8a7b89f04f77
Yurii Aksenov <Yura290610@yandex.ru>
2016-07-27 14:17:43 +0700

    Add OpenFolderDialogFrom

--- PhotoHelper/ViewModel/PathControlsViewModel.cs
+++ PhotoHelper/ViewModel/PathControlsViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +11,34 @@ using System.Windows.Input;
 
 namespace PhotoHelper.ViewModel
 {
-    class PathControlsViewModel
-    {
-        public ICommand OpenFolderDialog { get; set; }
+    public class PathControlsViewModel: ViewModelBase
+    { 
+        public ICommand OpenFolderDialogFromCommand { get; set; }
+        public string PathFrom { get; set; }
+
+        internal PathControlsViewModel()
+        {
+            OpenFolderDialogFromCommand = new RelayCommand(this.OpenFolderDialogFrom);
+        }
+
+        public void  OpenFolderDialogFrom()
+        {
+            try
+            {
+                var dialog = new CommonOpenFileDialog();
+
+                dialog.IsFolderPicker = true;
+                CommonFileDialogResult result = dialog.ShowDialog();
+                if (result.ToString() != "Cancel")
+                {
+                    PathFrom = dialog.FileName;
+                }
+            }
+            catch
+            {
+                throw new Exception("Не смог выбрать папку.");
+            }
+        }
+
     }
 }
